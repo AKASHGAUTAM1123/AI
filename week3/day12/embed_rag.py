6fb1862db0d225d7a21adfede352744756e73fad
AKASHGAUTAM1123 <akku.gautam23@gmail.com>
2026-08-19 15:18:58 +0530

    Added Day 12 embedding implementation

---
+++ week3/day12/embed_rag.py
@@ -0,0 +1,26 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+import numpy as np
+from sentence_transformers import SentenceTransformer
+
+def cosine_similarity(a, b):
+    return np.dot(a,b) / (
+        np.linalg.com(a) * np.linalg.norm(b)
+    )
+
+model = SentenceTransformer("all-MiniLM-L6-v2") #384
+text = "Machine learning is fun."
+
+# embedding=model.encode(text)
+# print(embedding.shape)
+# print(embedding[:10])
+
+
+t1="There are 24 paid leaves"
+t2="There are 24 vacation days"
+
+v1=model.encode(t1)
+v2=model.encode(t2)
+print(cosine_similarity(v1,v2))
